34cff3f1db03ae8020beba7ded9682d5c8b559f3
P NAGA VENKATA KUSHAL <182291393+nvk321@users.noreply.github.com>
2025-10-27 22:24:34 +0530

    Add Database Config file

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "bank_db",
+  multipleStatements: true
+});
+
+db.connect(err => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("✅ Connected to MySQL Database");
+  }
+});
+
+export default db;
